54f89934480f2dd3e92c09c3710c9bc4620180b7
Alexandr Ovsii <sedanvw@gmail.com>
2023-07-31 21:58:41 +0300

    task 3 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,14 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle'
 
-const iframe = document.querySelector('iframe');
-    const player = new Player(iframe);
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
 
-    player.on('timeupdate', throttle(function(data) {
-        localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds));
-    }), 1000);
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay ({ seconds }) {
+    localStorage.setItem("videoplayer-current-time", seconds);
+};
 
 const currentTime = localStorage.getItem("videoplayer-current-time");
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form'); //получаем доступ к форме
+form.addEventListener('input', throttle(onFormData, 500)); //вешаем обработчик событий на инпут и для ф-ции делаем ограничение частоты ее выполнения
+form.addEventListener('submit', onSubmitForm)
+
+const formData = {}; //задаем переменную для последующего хранения данных
+
+
+function onFormData(evt){ //ф-ция, которая вызывается при инпуте
+    formData[evt.target.name] = evt.target.value;//динамически присваиваем значение введенных данных в поле мейл и месседж
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData)) //задаем ключу значение полученным из форм-дата после метода stringify
+}
+
+function onSubmitForm(evt){
+    console.log(JSON.parse(localStorage.getItem('feedback-form-state'))); //получаем значение хранилища по ключу, потом парсим его в вадидный джс и выводим в консоль
+    evt.preventDefault(); 
+    evt.currentTarget.reset(); //сбрасываем данные
+    localStorage.removeItem("feedback-form-state"); //удаляем из хранилища
+}
+
+function dataFromLocalStorage() { //ф-ция для восстановления данных в полях формы при перезагрузке страницы
+    const data = JSON.parse(localStorage.getItem('feedback-form-state')); //получаем значение хранилища по ключу, потом парсим его в вадидный джс и присваиваем переменной
+    const email = document.querySelector('.feedback-form input'); //получаем доступ к значению, которое записано в инпут мейл
+    const message = document.querySelector('.feedback-form textarea');//получаем доступ к значению, которое записано в тексареа
+    if (data) { //если в дате есть данные
+      email.value = data.email; //присваивание в значение 
+      message.value = data.message; //присваивание в значение 
+    }
+  }
+
+  dataFromLocalStorage();
+
+
+
 
