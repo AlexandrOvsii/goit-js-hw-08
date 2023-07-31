591f52790473fc9390638e8bb2fc2c06c072beea
Alexandr Ovsii <sedanvw@gmail.com>
2023-07-31 18:03:28 +0300

    task 1 ready

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,30 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery'); //получаем доступ к классу gallery
 
-console.log(555);
+const markup = galleryItems //создаем переменную "разметка" и через мар перебираем наш galleryItems создавая необходимые эл-ты на каждой итерации.
+  .map(
+    item => `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+            <img 
+                class="gallery__image" 
+                src="${item.preview}" 
+                alt="${item.description}"
+            />
+        </a>
+    </li>`
+  )
+  .join(''); //объединяем все в одну строку
+
+galleryEl.insertAdjacentHTML('afterbegin', markup); //добавляем в ХТМЛ
+
+//==
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt', //вносим изменения в свойства
+  captionDelay: 250, //вносим изменения в свойства
+});
